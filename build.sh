6a2d849884a9105a84e6479db6e77f7f3acf7a7d
mgossman71 <mark.gossman71@gmail.com>
2020-12-10 05:28:42 -0600

    update

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=v1
+version=v1.1
 
 docker build . -t mgossman71/k8srelay:$version
 docker build . -t mgossman71/k8srelay:latest
@@ -7,5 +7,5 @@ docker build . -t mgossman71/k8srelay:latest
 docker push mgossman71/k8srelay:$version
 docker push mgossman71/k8srelay:latest
 
-kubectl create deployment -n api k8srelay --image=mgossman71/k8srelay
-kubectl expose deployment --port 8080 -n api k8srelay --type=NodePort --name=k8srelay-svc
+#kubectl create deployment -n api k8srelay --image=mgossman71/k8srelay
+#kubectl expose deployment --port 8080 -n api k8srelay --type=NodePort --name=k8srelay-svc
